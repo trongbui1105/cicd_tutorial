71d493783389f06467b3a0a3e90c7731f674337b
trongbui1105 <buiquoctrong110500@gmail.com>
2021-10-26 03:46:17 -0700

    second commit

--- flask_docker.py
+++ flask_docker.py
@@ -1,10 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
-
-
 @app.route('/')
-def hello_world():
-    return 'Hello FOSS'
 
+
+def hello_world():
+    return 'Hello FOSS, Hello CI/CD using Jenkins'
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
